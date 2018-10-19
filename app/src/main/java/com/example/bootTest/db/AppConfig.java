6cf2192835c741c01b447cd4cf663819880f8145
wanggu <guofu.wang@dianrong.com>
2018-10-19 14:26:26 +0800

    test

--- app/src/main/java/com/example/bootTest/db/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.bootTest.db;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.example.demo4.app.db.dao")
-public class AppConfig {
-}
